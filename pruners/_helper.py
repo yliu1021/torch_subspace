ddaf40d7f879c3d4f4af35d1b838af1495e63683
Siddharth Joshi <sjoshi804@gmail.com>
2022-06-01 14:45:54 -0700

    convert pruned sv to bias

--- pruners/_helper.py
+++ pruners/_helper.py
@@ -33,3 +33,42 @@ def _prune_scores(
         module.set_mask(mask.to(device=device))
         print(f"\rPruning module {i+1} / {len(prunable_modules)}", end="")
     print()
+
+def _convert_pruned_sv_to_bias(
+    model: nn.Module,
+    train_data: DataLoader,
+):
+    def accumulate_pruned_output_hook(module: nn.Module, input, output):
+        if isinstance(module, SubspaceLR) and module.is_leaf:
+            if module.accumulated_pruned_output is None:
+                module.accumulated_pruned_output = output 
+            else:
+                module.accumulated_pruned_output += output
+            return 
+    
+    def invert_mask(_model):
+        for module in _model.modules():
+            if isinstance(module, SubspaceLR) and module.is_leaf:
+                module.sv_mask = 1 - module.sv_mask
+    
+    invert_mask(model)
+
+    # registers hook globally
+    handle = torch.nn.modules.module.register_forward_hook(accumulate_pruned_output_hook)
+
+    # forward pass through data
+    num_batches = len(data)
+    model.eval()
+    with torch.no_grad():
+        for X, y in train_data:
+            X, y = X.to(device), y.to(device)
+            _ = model(X)
+
+    for module in model.modules():
+        if isinstance(module, SubspaceLR) and module.is_leaf:
+            pruned_sv_bias = torch.mean(module.accumulated_pruned_output / num_batches)
+            module.bias += pruned_sv_bias
+
+    # Remove hook and invert mask again to get original mask
+    handle.remove()
+    invert_mask(model)
