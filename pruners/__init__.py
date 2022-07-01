ab81853afca93fe95a80036b731f388ad3111d8c
Siddharth Joshi <sjoshi804@gmail.com>
2022-07-01 12:43:31 -0700

    alignment variance 2 added

--- pruners/__init__.py
+++ pruners/__init__.py
@@ -1,1 +1,1 @@
-from . import alignment_output, rel_error, magnitude, alignment_output_sampling, alignment_variance
+from . import alignment_output, rel_error, magnitude, alignment_output_sampling, alignment_variance, alignment_variance_2
